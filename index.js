6e114a455bb254678ffe5a979ebe9206807e5fe1
Christian <christiansmith2394@gmail.com>
2021-09-10 18:26:04 -0400

    I'M STUCK!

--- index.js
+++ index.js
@@ -168,8 +168,8 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(array) {
-  return `${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`
+function getLastReview(reviews) {
+  return `${reviews[reviews.length - 1].name} gave the restaurant a ${reviews[reviews.length - 1].rating} star review, and their feedback was: ${reviews[reviews.length - 1].feedback}`
   
 } 
 
